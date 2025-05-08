67d17fc4fbcefb4b49fe4f1f2df7b8051d814e95
devon-artmeier <devon.artmeier@outlook.com>
2025-05-07 23:49:21 -0400

    Set up cart test source

---
+++ framework/common.mk
@@ -0,0 +1,24 @@
+# ------------------------------------------------------------------------------
+# Tools
+# ------------------------------------------------------------------------------
+
+VASM                 := vasmm68k_mot
+VLINK                := vlink
+MKASMDEP             := mkasmdep
+DUMPASMSYM           := dumpasmsym
+
+VASM_FLAGS           := -Fvobj -quiet -ldots -spaces
+VLINK_FLAGS          := -b rawbin
+MKASMDEP_FLAGS       := -r
+DUMPASMSYM_FLAGS     := -m asm
+
+# ------------------------------------------------------------------------------
+# Messages
+# ------------------------------------------------------------------------------
+
+DEPEND_MSG            = @echo Generating $@
+ASSEMBLE_MSG          = @echo Assembling $<
+LINK_MSG              = @echo Linking    $@
+EXPORT_MSG            = @echo Exporting  $@
+
+# ------------------------------------------------------------------------------
